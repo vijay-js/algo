b5a5a0f2a0679e7811ca13b41af3496bdc9fc16b
Vijay <vijay4git@gmail.com>
2020-10-23 07:50:24 +0530

    Sort Stack by Recursion

--- TheMission/src/RecursionLearn/basics/Fibonacci.java
+++ TheMission/src/RecursionLearn/basics/Fibonacci.java
@@ -1,10 +1,12 @@
 package RecursionLearn.basics;
 
+import Logger.Log;
+
 public class Fibonacci {
     public static void main(String[] args) {
 
-
-
+        Log.write(fib(4));
+        Log.write(factIter(4));
 
     }
 
@@ -16,5 +18,17 @@ public class Fibonacci {
         return fib(n-1) + fib(n-2);
     }
 
+    public static int factIter(int n) {
+        if(n == 0 || n == 1) return 1;
+        int prev = 1;
+        int current = 1;
+        for(int i=2;i<=n;i++) {
+            int temp = prev + current;
+            prev = current;
+            current = temp;
+        }
+        return current;
+
+    }
 
 }
---
+++ TheMission/src/RecursionLearn/basics/SortStackByRecursion.java
@@ -0,0 +1,54 @@
+package RecursionLearn.basics;
+
+import java.util.Stack;
+
+public class SortStackByRecursion {
+    public static void main(String[] args) {
+        Stack<Integer> s = new Stack<>();
+        s.push(6);
+        s.push(5);
+        s.push(10);
+        s.push(1);
+        s.push(2);
+        printStack(s);
+        sortStack(s,s.size());
+        System.out.println();
+        printStack(s);
+    }
+
+
+    public static void sortStack(Stack<Integer> s, int n) {
+        if(n <= 1) return;
+
+        int el = s.pop();
+
+        sortStack(s,n-1);
+
+        insertIntoSortedStack(s,el);
+    }
+
+
+
+    public static void insertIntoSortedStack(Stack<Integer> s, int el) {
+        if(s.size() == 0 || s.peek() <= el) {
+            s.push(el);
+            return;
+        }
+        int val = s.pop();
+
+        insertIntoSortedStack(s,el);
+
+        s.push(val);
+
+    }
+
+
+    public static void printStack(Stack<Integer> s) {
+        if(s.isEmpty()) return;
+
+        int val = s.pop();
+        System.out.print(val + " ");
+        printStack(s);
+        s.push(val);
+    }
+}
